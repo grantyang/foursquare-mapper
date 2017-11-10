a6ec059c1e139dab444b4684619c8f392a4c22f3
Grant Yang <grantyang1@gmail.com>
2017-11-10 18:25:02 +0100

    github pages testing

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Foursquare Mapper</h1>
+          <h1 className="App-title">Proximap</h1>
         </header>
         <p className="App-intro">
-          
+          Hello World!
         </p>
       </div>
     );
