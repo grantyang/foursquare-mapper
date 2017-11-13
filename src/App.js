d30e23775d8d8f6524b558c3227b8d5e23d9f6d6
Grant Yang <grantyang1@gmail.com>
2017-11-13 23:03:03 +0100

    add basic API query functionality

--- src/App.js
+++ src/App.js
@@ -1,18 +1,74 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Input from './presentational/input.js';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: []
+    };
+  }
+
+  componentDidMount() {
+
+  }
+
+  // addSearchInputToList = searchInput => {
+  //   //adds new todo item to this list
+  //   if (!searchInput) {
+  //     return alert('Please input a value');
+  //   } else {
+  //     console.log(searchInput);
+  //   }
+  // };
+
+  search = searchInput => {
+    const query = searchInput;
+    const section = '';
+    const city = 'san francisco';
+    const limit = 50;
+    fetch(
+      `http://localhost:5000/fsquare/explore?query=${query}&section=${section}&near=${city}&limit=${limit}`,
+      {
+        method: 'GET'
+      }
+    )
+      .then(res => res.json())
+      .then(data => {
+        console.log(data.response.groups[0]);
+        this.setState({ items: data.response.groups[0].items });
+      });
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Proximap</h1>
-        </header>
-        <p className="App-intro">
-          Hello World!
-        </p>
+      <div>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Proximap</h1>
+          </header>
+        </div>
+        <div className="container">
+          <Input fxToRun={this.search} />
+          <div className="row justify-content-sm-center">
+            <div>Items:</div>
+          </div>
+          {this.state.items.map(item => {
+            return (
+              <div
+                className="row justify-content-sm-center"
+                key={item.venue.id}>
+                {item.venue.name} //{' '}
+                {item.venue.categories[0] &&
+                  item.venue.categories[0].shortName}{' '}
+                // {item.venue.rating} // {item.venue.stats.checkinsCount}
+              </div>
+            );
+          })}
+        </div>
       </div>
     );
   }
---
+++ src/presentational/input.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+
+export default class Input extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { text: '' }; // init state to blank
+  }
+
+  onInputChange = event => {
+    // when input is changed, update state
+    this.setState({ text: event.target.value });
+  };
+
+  onInputSubmit = event => {
+    // when input is submitted, run fx
+
+    event.preventDefault();
+    this.props.fxToRun(this.state.text);
+    this.setState({ text: '' }); //clears out input after submit
+  };
+
+  render() {
+    return (
+      <div className="row">
+        <form
+          className="input input-group justify-content-center"
+          onSubmit={this.onInputSubmit}>
+          <input
+            type="text"
+            className="form-control col-sm-4"
+            placeholder="Enter text here"
+            value={this.state.text} // grab value from state
+            onChange={this.onInputChange} // update state on change
+          />
+          <span className="input-group-btn">
+            <button className="btn btn-success" type="submit">
+              Add
+            </button>
+          </span>
+        </form>
+      </div>
+    );
+  }
+}
